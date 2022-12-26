f8be655ee209a11b8a836fbad49721227e98a061
abdoachhoubi <aachhoub@gmail.com>
2022-12-26 20:24:04 +0100

    Added open door check

--- src/check.c
+++ src/check.c
@@ -5,15 +5,24 @@ int	key_pressed(int code, t_game *game)
 {
 	if (code == ESC)
 		destroy_win(game);
-	if (code == UP)
+	if (code == UP1 || code == UP2)
 		up(game);
-	if (code == RIGHT)
+	if (code == RIGHT1 || code == RIGHT2)
 		right(game);
-	if (code == DOWN)
+	if (code == DOWN1 || code == DOWN2)
 		down(game);
-	if (code == LEFT)
+	if (code == LEFT1 || code == LEFT2)
 		left(game);
 	return (0);
+}
+
+void	open_exit(t_game *game)
+{
+	t_image	image;
+
+	image.path = "./res/floor.xpm";
+	image.game = *game;
+	put_image(image, game->exit_coordinates[0], game->exit_coordinates[1]);
 }
 
 // Checks the outcome of a move
@@ -25,12 +34,14 @@ int	move_check(t_game *game, int i, int j)
 	{
 		game->player.coin++;
 		game->map[i][j] = '0';
+		if (game->check_coin == game->player.coin)
+			open_exit(game);
 	}
 	else if (game->map[i][j] == 'E')
 	{
 		if (game->player.coin != game->check_coin)
 		{
-			ft_putendl_fd(RED"Bonus collected\n"RESET, 1);
+			ft_putendl_fd(RED"You must collect all coins"RESET, 1);
 			return (1);
 		}
 		message(GREEN"Congratulations uwu\n"RESET, 1, game);
