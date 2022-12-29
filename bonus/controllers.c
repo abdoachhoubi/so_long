36e7dc9b9cd04b37f88233f992255296453c5c1a
abdoachhoubi <aachhoub@gmail.com>
2022-12-29 16:05:02 +0100

    Separated controllers and counters

--- bonus/controllers.c
+++ bonus/controllers.c
@@ -12,34 +12,6 @@
 
 #include "../inc/so_long.h"
 
-void	clear_text(t_game *game)
-{
-	t_image	image;
-	size_t	i;
-
-	image.path = "./res/wall.xpm";
-	image.game = *game;
-	i = 0;
-	while (i <= game -> x)
-	{
-		put_image(image, i, game -> y);
-		i += SIZE;
-	}
-}
-
-void	display_count(t_game *game, int count, int bonus)
-{
-	char	*s;
-
-	clear_text(game);
-	s = ft_strjoin("Moves count = ", ft_itoa(count));
-	mlx_string_put(game -> mlx, game -> win, game -> x / 3, game -> y + 12, 16777215, s);
-	free(s);
-	s = ft_strjoin("Collectibles = ", ft_itoa(bonus));
-	mlx_string_put(game -> mlx, game -> win, (game -> x / 3) * 2, game -> y + 12, 16777215, s);
-	free(s);
-}
-
 // Updates the map
 void	update(t_game *game, int x, int y)
 {
@@ -57,7 +29,7 @@ void	right(t_game *game)
 	int		j;
 	t_image	image;
 
-	image.path = "./res/player.xpm";
+	image.path = "./res/right.xpm";
 	image.game = *game;
 	i = game->player.y;
 	j = game->player.x + SIZE;
@@ -101,7 +73,7 @@ void	up(t_game *game)
 	int		j;
 	t_image	image;
 
-	image.path = "./res/player.xpm";
+	image.path = "./res/hero.xpm";
 	image.game = *game;
 	i = game->player.y - SIZE;
 	j = game->player.x;
@@ -123,7 +95,7 @@ void	down(t_game *game)
 	int		j;
 	t_image	image;
 
-	image.path = "./res/player.xpm";
+	image.path = "./res/hero.xpm";
 	image.game = *game;
 	i = game->player.y + SIZE;
 	j = game->player.x;
